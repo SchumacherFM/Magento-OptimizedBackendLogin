396ebf845b9dc5f6f96d5e0d3878f0ba6404ac45
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-06-03 22:32:06 +0200

    Modul functionality

--- src/app/code/community/SchumacherFM/OptBeLogin/Helper/Data.php
+++ src/app/code/community/SchumacherFM/OptBeLogin/Helper/Data.php
@@ -8,6 +8,76 @@
  */
 class SchumacherFM_OptBeLogin_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    const XML_PATH_PRECHECKED_ATTRIBUTES_PRODUCT = 'dev/optbelogin/prechecked_attributes_product';
+
+    protected $_versionMapper = array(
+        '1.13' => '1.8',
+        '1.8'  => '1.8',
+    );
+
+    const XML_PATH_DOMAIN_PREFIX = 'dev/optbelogin/domain_';
+
+    public function getAutoComplete()
+    {
+        $off = 'off';
+        $on  = 'on';
+
+        if ($this->isDomainArea('local') || $this->isDomainArea('acc')) {
+            return $on;
+        }
+
+        return $off;
+    }
+
+    /**
+     * @param string $area [local|acc]
+     *
+     * @return string
+     */
+    public function isDomainArea($area = 'local')
+    {
+        return stristr($_SERVER['HTTP_HOST'], Mage::getStoreConfig(self::XML_PATH_DOMAIN_PREFIX . $area)) !== FALSE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMagentoEdition()
+    {
+        return 'ce';
+    }
+
+    /**
+     * @return string
+     */
+    public function getMagentoVersion()
+    {
+        $versionInfo = Mage::getVersionInfo();
+        $version     = $versionInfo['major'] . '.' . $versionInfo['minor'];
+        return isset($this->_versionMapper[$version]) ? $this->_versionMapper[$version] : '1.8';
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDisableForgotPasswordLink()
+    {
+        return Mage::getStoreConfigFlag('dev/optbelogin/disable_forgot_password_link');
+    }
+
+    /**
+     * @return bool
+     */
+    public function getDisableLogo()
+    {
+        return Mage::getStoreConfigFlag('dev/optbelogin/disable_logo');
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFormColor()
+    {
+        return Mage::getStoreConfig('dev/optbelogin/form_color');
+    }
 
 }
---
+++ src/app/code/community/SchumacherFM/OptBeLogin/Model/Observer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author kiri
+ * @date   6/3/13
+ */
+
+class SchumacherFM_OptBeLogin_Model_Observer
+{
+
+    public function setAdminLoginTemplate(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Adminhtml_Block_Template $block */
+        $block = $observer->getEvent()->getBlock();
+
+        if (!$block instanceof Mage_Adminhtml_Block_Template || $block->getTemplate() !== 'login.phtml') {
+            return null;
+        }
+
+        /** @var SchumacherFM_OptBeLogin_Helper_Data $helper */
+        $helper = Mage::helper('optbelogin');
+
+        $templatePath = array(
+            'optbelogin',
+            $helper->getMagentoEdition(),
+            $helper->getMagentoVersion(),
+            'login.phtml'
+        );
+
+        $block
+            ->setTemplate(implode(DS, $templatePath))
+            ->setAutoComplete($helper->getAutoComplete())
+            ->setDisableForgotPasswordLink($helper->getDisableForgotPasswordLink())
+            ->setDisableLogo($helper->getDisableLogo())
+            ->setFormColor($helper->getFormColor());
+
+    }
+
+}
